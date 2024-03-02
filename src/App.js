c2c8edb177d19f8d0d5463910ea5e20018e6cff1
kunal-batra <kunalbtr49@gmail.com>
2024-03-02 11:20:28 +0530

    remove edit

--- src/App.js
+++ src/App.js
@@ -4,91 +4,106 @@ import TaskList from "./components/taskList";
 import CompletedTask from "./components/completedTasks";
 
 function App() {
-    const [inputValue, setInputValue] = useState("");
-    const [taskList, setTaskList] = useState([]);
-    const [completedTaskList, setCompletedTaskList] = useState([]);
-    const [newTask, setNewTask] = useState();
+	const [inputValue, setInputValue] = useState("");
+	const [taskList, setTaskList] = useState([]);
+	const [completedTaskList, setCompletedTaskList] = useState([]);
+	const [newTask, setNewTask] = useState();
 
-    const inputElement = useRef(); // getting a virtual DOM element
+	const inputElement = useRef(); // getting a virtual DOM element
 
-    function handleChange(event) {
-        setInputValue(event.target.value); // Asynchronous
-    }
+	function handleChange(event) {
+		setInputValue(event.target.value); // Asynchronous
+	}
 
-    function handleClick() {
-        const newTaskList = [...taskList]; // Storing old tasks in a new variable
-        newTaskList.push(inputValue);
-        setNewTask(inputValue)
-        inputElement.current.value = "";
-        setTaskList(newTaskList);
-    }
+	function handleClick() {
+		const newTaskList = [...taskList]; // Storing old tasks in a new variable
+		newTaskList.push(inputValue);
+		setNewTask(inputValue);
+		inputElement.current.value = "";
+		setTaskList(newTaskList);
+	}
 
-    function doneClick(index) {
-        const newTaskList = [];
-        let newCompletedTaskList = [...completedTaskList];
-        for (let i = 0; i < taskList.length; i++) {
-            if (i !== index) {
-                newTaskList.push(taskList[i]);
-            } else {
-                newCompletedTaskList.push(taskList[i]);
-            }
-        }
-        setTaskList(newTaskList);
-        setCompletedTaskList(newCompletedTaskList);
-    }
+	function doneClick(index) {
+		const newTaskList = [];
+		let newCompletedTaskList = [...completedTaskList];
+		for (let i = 0; i < taskList.length; i++) {
+			if (i !== index) {
+				newTaskList.push(taskList[i]);
+			} else {
+				newCompletedTaskList.push(taskList[i]);
+			}
+		}
+		setTaskList(newTaskList);
+		setCompletedTaskList(newCompletedTaskList);
+	}
 
-    function deleteClick(listName, index) {
-        const isDelete = window.confirm("Are you sure you want to delete?");
-        if (isDelete) {
-            if (listName === "taskList") {
-                const newTaskList = [];
-                for (let i = 0; i < taskList.length; i++) {
-                    if (i !== index) {
-                        newTaskList.push(taskList[i]);
-                    }
-                }
-                setTaskList(newTaskList);
-            } else {
-                const newCompletedTaskList = [];
-                for (let i = 0; i < completedTaskList.length; i++) {
-                    if (i !== index) {
-                        newCompletedTaskList.push(completedTaskList[i]);
-                    }
-                }
-                setCompletedTaskList(newCompletedTaskList);
-            }
-        }
-    }
+	function undoClick(index) {
+		const newTaskList = [...taskList];
+		let newCompletedTaskList = [];
+		for (let i = 0; i < completedTaskList.length; i++) {
+			if (i !== index) {
+				newCompletedTaskList.push(completedTaskList[i]);
+			} else {
+				newTaskList.push(completedTaskList[i]);
+			}
+		}
+		setTaskList(newTaskList);
+		setCompletedTaskList(newCompletedTaskList);
+	}
 
-    return (
-        <div className="App">
-            <h1 className="app-header">Welcome to your To-Do List</h1>
-            <div className="container">
-                <div className="add-task">
-                    <input
-                        type="text"
-                        placeholder="Add a new task"
-                        onChange={handleChange}
-                        defaultValue={inputValue}
-                        ref={inputElement}
-                    />
-                    <button onClick={handleClick}>Add</button>
-                </div>
-                <div className="taskListContainer">
-                    <TaskList
-                        taskList={taskList}
-                        doneClick={doneClick}
-                        deleteClick={deleteClick}
-                        newTask={newTask}
-                    />
-                    <CompletedTask
-                        completedTaskList={completedTaskList}
-                        deleteClick={deleteClick}
-                    />
-                </div>
-            </div>
-        </div>
-    );
+	function deleteClick(listName, index) {
+		const isDelete = window.confirm("Are you sure you want to delete?");
+		if (isDelete) {
+			if (listName === "taskList") {
+				const newTaskList = [];
+				for (let i = 0; i < taskList.length; i++) {
+					if (i !== index) {
+						newTaskList.push(taskList[i]);
+					}
+				}
+				setTaskList(newTaskList);
+			} else {
+				const newCompletedTaskList = [];
+				for (let i = 0; i < completedTaskList.length; i++) {
+					if (i !== index) {
+						newCompletedTaskList.push(completedTaskList[i]);
+					}
+				}
+				setCompletedTaskList(newCompletedTaskList);
+			}
+		}
+	}
+
+	return (
+		<div className="App">
+			<h1 className="app-header">Welcome to your To-Do List</h1>
+			<div className="container">
+				<div className="add-task">
+					<input
+						type="text"
+						placeholder="Add a new task"
+						onChange={handleChange}
+						defaultValue={inputValue}
+						ref={inputElement}
+					/>
+					<button onClick={handleClick}>Add</button>
+				</div>
+				<div className="taskListContainer">
+					<TaskList
+						taskList={taskList}
+						doneClick={doneClick}
+						deleteClick={deleteClick}
+						newTask={newTask}
+					/>
+					<CompletedTask
+						completedTaskList={completedTaskList}
+						deleteClick={deleteClick}
+						undoClick={undoClick}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/taskList.js
+++ src/components/taskList.js
@@ -1,86 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 function TaskList(props) {
-    const [clickedIndex, setClickedIndex] = useState(-1);
-    const [inputValue, setInputValue] = useState("");
-    const [taskList, setTaskList] = useState([]);
-
-    useEffect(() => {
-        if (props.newTask) {
-            const newTaskList = [...taskList];
-            newTaskList.push(props.newTask);
-            setTaskList(newTaskList);
-        }
-    }, [props.newTask]);
-
-    function editClick(index) {
-        setClickedIndex(index);
-    }
-
-    function saveClick() {
-        const updatedTaskList = [...taskList];
-        updatedTaskList[clickedIndex] = inputValue;
-        setTaskList(updatedTaskList);
-        setClickedIndex(-1);
-    }
-
-    function handleChange(event) {
-        setInputValue(event.target.value);
-    }
-
-    function deleteClick(index) {
-        const isDelete = window.confirm("Are you sure you want to delete?");
-        if (isDelete) {
-            const newTaskList = [];
-            for (let i = 0; i < taskList.length; i++) {
-                if (i !== index) {
-                    newTaskList.push(taskList[i]);
-                }
-            }
-            setTaskList(newTaskList);
-        }
-    }
-
-    return (
-        <div className="task-list">
-            <h3 className="task-list-header">Task List</h3>
-            {taskList.length === 0 ? (
-                <p className="no-task">No Task added in the list</p>
-            ) : (
-                taskList.map((task, index) => {
-                    return (
-                        <div className="taskDiv" key={index}>
-                            {index === clickedIndex ? (
-                                <>
-                                    <input
-                                        className="edit-field"
-                                        defaultValue={task}
-                                        onChange={handleChange}
-                                    />
-                                    <button onClick={saveClick}>Save</button>
-                                </>
-                            ) : (
-                                <>
-                                    <p className="taskName">{task}</p>
-                                    <button
-                                        onClick={() => props.doneClick(index)}
-                                    >
-                                        Done
-                                    </button>
-                                    <button onClick={() => editClick(index)}>
-                                        Edit
-                                    </button>
-                                    <button onClick={() => deleteClick(index)}>
-                                        Delete
-                                    </button>
-                                </>
-                            )}
-                        </div>
-                    );
-                })
-            )}
-        </div>
-    );
+	return (
+		<div className="task-list">
+			<h3 className="task-list-header">Task List</h3>
+			{props.taskList.length === 0 ? (
+				<p className="no-task">No Task added in the list</p>
+			) : (
+				props.taskList.map((task, index) => {
+					return (
+						<div className="taskDiv" key={index}>
+							{index === "clickedIndex" ? (
+								<>
+									{/* <input
+										className="edit-field"
+										defaultValue={task}
+										onChange={handleChange}
+									/>
+									<button onClick={saveClick}>Save</button> */}
+								</>
+							) : (
+								<>
+									<p className="taskName">{task}</p>
+									<button
+										onClick={() => props.doneClick(index)}
+									>
+										Done
+									</button>
+									{/* <button onClick={() => editClick(index)}>
+										Edit
+									</button> */}
+									<button
+										onClick={() =>
+											props.deleteClick("taskList", index)
+										}
+									>
+										Delete
+									</button>
+								</>
+							)}
+						</div>
+					);
+				})
+			)}
+		</div>
+	);
 }
 
 export default TaskList;
